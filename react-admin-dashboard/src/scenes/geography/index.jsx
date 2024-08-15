746ce0ae67c8bc8ce33ec3100c3e26366973c2e9
kota-karthik <kotakarthik2307@gmail.com>
2024-08-15 23:58:05 +0530

    integration finished

--- react-admin-dashboard/src/scenes/geography/index.jsx
+++ react-admin-dashboard/src/scenes/geography/index.jsx
@@ -8,7 +8,7 @@ const Geography = () => {
   const colors = tokens(theme.palette.mode);
   return (
     <Box m="20px">
-      <Header title="Geography" subtitle="Simple Geography Chart" />
+      <Header title="Geography" subtitle="Geographic sales distribution" />
 
       <Box
         height="75vh"
